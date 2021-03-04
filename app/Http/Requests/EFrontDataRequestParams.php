bc53349e0cd9f97cd2065e9c76f57c8aeb3794c6
Voronin Vitaliy <paxexterminatus@gmail.com>
2021-03-04 13:44:56 +0300

    efront

--- app/Http/Requests/EFrontDataRequestParams.php
+++ app/Http/Requests/EFrontDataRequestParams.php
@@ -15,8 +15,8 @@ class EFrontDataRequestParams
     public function __construct(array $requestAll)
     {
         $this->clientCode = $requestAll['cl_code'] ?? null;
-        $this->requestType = $requestAll['rq_type'] ?? null;
-        $this->requestCode = $requestAll['rq_code'] ?? null;
+        $this->requestType = $requestAll['type'] ?? null;
+        $this->requestCode = $requestAll['code'] ?? null;
         $this->nodeId = $requestAll['node_id'] ?? null;
         $this->lessonItem = $requestAll['lesson_item'] ?? null;
     }
