41471b1354a042a0bd577d072a6324154549dce3
Voronin Vitaliy <PaxExterminatus@gmail.com>
2024-04-06 21:57:52 +0300

    updated AppMenu

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,20 +1,19 @@
 <template>
     <header class="app-header">
         <Button icon="pi pi-bars" severity="secondary" raised @click="menu.show()"/>
-        <AppMenu/>
     </header>
 
     <div class="app-body">
-
         <div class="main-content">
             <router-view/>
         </div>
     </div>
+
+    <AppMenuComponent/>
 </template>
 
 <script setup>
-import AppMenu from 'cmp/template/AppMenu/AppMenuComponent'
-import {menu} from 'app/lib'
-
 import Button from 'primevue/button'
+import {menu, AppMenuComponent} from 'cmp/template/AppMenu'
+
 </script>
