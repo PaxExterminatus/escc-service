1e1f8ad398208cadc1fe2b4c36b37372b610de65
Voronin Vitaliy <PaxExterminatus@gmail.com>
2024-10-21 23:43:33 +0300

    escc-service-v2.0 added profile

--- app/Casts/SexCast.php
+++ app/Casts/SexCast.php
@@ -12,14 +12,14 @@ class SexCast implements CastsAttributes
     public function get(Model $model, string $key, mixed $value, array $attributes): string
     {
         if ((int)$value === 1) return SexEnum::man->value;
-        else if ((int)$value === 0) return SexEnum::woman->value;
+        elseif ((int)$value === 0) return SexEnum::woman->value;
         else return SexEnum::unknown->value;
     }
 
     public function set(Model $model, string $key, mixed $value, array $attributes): ?int
     {
         if (Str::lower($value) === SexEnum::man->value) return 1;
-        else if (Str::lower($value) === SexEnum::woman->value) return 0;
+        elseif (Str::lower($value) === SexEnum::woman->value) return 0;
         else return null;
     }
 }
