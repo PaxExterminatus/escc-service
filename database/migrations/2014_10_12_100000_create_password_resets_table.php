f69a5b752fcf80a174fec073d6bc777b2c50f5f5
Voronin Vitaliy <PaxExterminatus@gmail.com>
2026-09-08 14:52:13 +0300

    container update

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -6,11 +6,8 @@ use Illuminate\Support\Facades\Schema;
 
 class CreatePasswordResetsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+/*
+
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
@@ -20,13 +17,9 @@ class CreatePasswordResetsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('password_resets');
     }
+ */
 }
